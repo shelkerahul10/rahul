2978de7ea1983252fa0b84d95f26561b6bcf5728
shelkerahul10 <shelkerahul307@gmail.com>
2017-02-26 17:53:11 +0530

    Update hello.java

--- Helloworld/src/hello.java
+++ Helloworld/src/hello.java
@@ -5,6 +5,8 @@ public class hello {
 		// TODO Auto-generated method stub
 System.out.println("Hello world ");
 System.out.println("wow");
+		System.Out.Println("repository to eclips");
+		System.out.Println("Second Print");
 System.out.println("welcome to java program");
 	}
 
