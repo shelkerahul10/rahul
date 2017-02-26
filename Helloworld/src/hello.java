9bfcb498f3f5bd5b3138f131048619e639b696ac
rahul <rahul@rahul>
2017-02-26 17:44:04 +0530

    second commit

--- Helloworld/src/hello.java
+++ Helloworld/src/hello.java
@@ -4,6 +4,8 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello world ");
+System.out.println("wow");
+System.out.println("welcome to java program");
 	}
 
 }
